3a296f99be34329ba6132cc5a54fd14add95f28f
Jeff Crockett <jeffmcrockett@gmail.com>
2021-05-06 19:17:25 -0500

    setup skeleton of app

---
+++ connection.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql');
+
+let connection = mysql.createConnection({
+  host: 'ec2-54-87-112-29.compute-1.amazonaws.com',
+  user: 'mgujwfkuppyvbo',
+  password: 'ab17a2b5b15b9e01fd17ea2e55062e19981f79f76152e482722750a0692d142c',
+  database: 'd701ft5q0sf4mu'
+});
+
+connection.connect(function(err) {
+  if(err){
+    console.log('error connecting : ' + err.stack);
+    return;
+  };
+  console.log('connected as id ' + connection.threadId)
+});
+
+module.exports = connection;
--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Welcome to BidPros");
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4001
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(require("./router/router"));
+
+app.get('/', (req, res) => res.send('default route'))
+
+app.listen(port, () => {
+  console.log('App is listening on:', port)
+})
