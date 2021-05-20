b52280495dad440c85fa53fdcb92fdbe7be18622
Jeff Crockett <jeffmcrockett@gmail.com>
2021-05-20 10:25:48 -0500

    trying to figure out auth0, looking for tutoring this weekend

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+const { logger } = require('./authenticate/authenticate.js');
+const router = require('./router/router.js');
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+app.use(logger);  
+app.use(router);
 
 app.use(require('./router/router'));
 
--- router/router.js
+++ router/router.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const controller = require("../controller/controller.js")
 const authenticate = require("../authenticate/authenticate.js")
+const authController = require('../controller/auth.js');
 
 router.post("/createEvent", authenticate.checkJwt, controller.createEvent);
 
@@ -20,5 +21,7 @@ router.post('/createUser', authenticate.checkJwt, controller.createUser);
 router.put('/updateUser/users/:user_id', authenticate.checkJwt, controller.updateUser);
 
 router.delete('/delete/items/:item_id', authenticate.checkJwt, controller.deleteItem);
+
+router.post('/auth/login', authController.login); 
 
 module.exports = router;
