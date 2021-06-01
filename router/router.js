f3208078a21bcafba005e4629ea3c96cd8c7878f
Jeff Crockett <jeffmcrockett@gmail.com>
2021-06-01 18:11:49 -0500

    removed authentication

--- router/router.js
+++ router/router.js
@@ -1,27 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const controller = require("../controller/controller.js")
-const authenticate = require("../authenticate/authenticate.js")
-const authController = require('../controller/auth.js');
+// const authenticate = require("../authenticate/authenticate.js")
+// const authController = require('../controller/auth.js');
 
-router.post("/createEvent", authenticate.checkJwt, controller.createEvent);
+router.post("/createEvent", controller.createEvent);
 
-router.post('/createItem', authenticate.checkJwt, controller.createItem);
+router.post('/createItem', controller.createItem);
 
-router.put('/updateItem/:item_id', authenticate.checkJwt, controller.updateItem);
+router.put('/updateItem/:item_id', controller.updateItem);
 
-router.get("/events/:event_id/items", authenticate.checkJwt, controller.getAllItemsByEvent);
+router.get("/events/:event_id/items", controller.getAllItemsByEvent);
 
-router.get("/events/:event_id/users", authenticate.checkJwt, controller.getAllUsersByEvent);
+router.get("/events/:event_id/users", controller.getAllUsersByEvent);
 
 router.get("/events", controller.getAllEvents);
 
-router.post('/createUser', authenticate.checkJwt, controller.createUser);
+router.post('/createUser', controller.createUser);
 
-router.put('/updateUser/users/:user_id', authenticate.checkJwt, controller.updateUser);
+router.put('/updateUser/users/:user_id', controller.updateUser);
 
-router.delete('/delete/items/:item_id', authenticate.checkJwt, controller.deleteItem);
+router.delete('/delete/items/:item_id', controller.deleteItem);
 
-router.post('/auth/login', authController.login); 
+// router.post('/auth/login', authController.login); 
 
 module.exports = router;
